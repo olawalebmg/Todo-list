4cbb9dbd3ef577ea0455e209517afcc16f1d6865
olawalebmg <olawalebmg@outlook.com>
2021-05-25 23:07:34 +0100

    firebase

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBMz0SZ56WZ8Mb1JvlFriZNHenH6XTvmII",
+    authDomain: "project-26610.firebaseapp.com",
+    projectId: "project-26610",
+    storageBucket: "project-26610.appspot.com",
+    messagingSenderId: "30086224137",
+    appId: "1:30086224137:web:71615e3404ca41407a9bd8",
+    measurementId: "G-3WMZ5MCL77"
+  }
 };
 
 /*
